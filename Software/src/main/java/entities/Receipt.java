825a0300afa2346093e8d82112d4cd98ddb2c141
tranhaikimlong <kimlong.speed8993@gmail.com>
2021-03-06 16:37:23 +0700

    Create Receipt, Roles, RolesDetail

---
+++ Software/src/main/java/entities/Receipt.java
@@ -0,0 +1,71 @@
+package entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "receipt")
+public class Receipt {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "order_id")
+    private String orderId;
+    @Column(name = "employee_id")
+    private String employeeId;
+    @Column(name = "created_date")
+    private Date createdDate;
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    public Receipt(){
+
+    }
+
+    public Receipt(String id, String orderId, String employeeId, Date createdDate, Date updateDate){
+        this.id = id;
+        this.orderId = orderId;
+        this.employeeId = employeeId;
+        this.createdDate = createdDate;
+        this.updateDate = updateDate;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderId(){
+        return orderId;
+    }
+    public void setOrderId(String order_id) {
+        this.orderId = orderId;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+    public void setEmployeeId(String employee_id) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
---
+++ Software/src/main/java/entities/Roles.java
@@ -0,0 +1,60 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "created_date")
+    private Date createdDate;
+    @Column(name = "updated_date")
+    private Date updatedDate;
+
+    public Roles(){
+
+    }
+
+    public Roles(String id, String name, Date createdDate, Date updatedDate){
+        this.id = id;
+        this.name = name;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
