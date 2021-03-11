f9b8fb9a36438d75234b321e0ef71b7c2edf652f
ngmdatt <72783931+ngmdatt@users.noreply.github.com>
2021-03-11 19:46:53 +0700

    Build Customer Category, Customer Add UI

--- Software/src/main/java/controllers/CustomerCategoryController.java
+++ Software/src/main/java/controllers/CustomerCategoryController.java
@@ -1,4 +1,42 @@
 package controllers;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import utils.StageHelper;
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class CustomerCategoryController {
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private JFXButton searchButton;
+
+    @FXML
+    private JFXButton addButton;
+
+    @FXML
+    void addButtonActionListener(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/CustomerAdd.fxml")));
+            StageHelper.startStage(root);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+        }
+    }
+
+    @FXML
+    void searchButtonActionListener(ActionEvent event) {
+
+    }
+
 }
