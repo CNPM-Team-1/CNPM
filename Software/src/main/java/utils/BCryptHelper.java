049ff336c73dd9b06abf5d02ad384e423e221baf
TAN <trantrungtan712000@gmail.com>
2021-03-13 20:19:32 +0700

    Build salary UI

--- Software/src/main/java/utils/BCryptHelper.java
+++ Software/src/main/java/utils/BCryptHelper.java
@@ -1,13 +1,15 @@
-package utils;
-
-import org.mindrot.jbcrypt.BCrypt;
-
-public class BCryptHelper {
-    public static boolean check(String text, String hashed) {
-        return BCrypt.checkpw(text, hashed);
-    }
-
-    public static String encode(String text) {
-        return BCrypt.hashpw(text, BCrypt.gensalt(9));
-    }
-}
+//package utils;
+//
+//import org.mindrot.jbcrypt.BCrypt;
+//
+//public class BCryptHelper {
+//    public static boolean check(String text, String hashed) {
+//        return BCrypt.checkpw(text, hashed);
+//    }
+//
+//    public static String encode(String text) {
+//        return BCrypt.hashpw(text, BCrypt.gensalt(9));
+//    }
+//}
+// con nếu cái này bị lỗi: chuột phải pom.xml -> maven -> reload project
+// tạm che đi, chưa cần dùng đâu
