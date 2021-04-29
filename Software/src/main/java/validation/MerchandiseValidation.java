589cb522749dae97fd4b08d05d33d08adb9a517d
haihuy246 <72715238+haihuy246@users.noreply.github.com>
2021-04-29 21:26:56 +0700

    Update MerchandiseValidation.java

--- Software/src/main/java/validation/MerchandiseValidation.java
+++ Software/src/main/java/validation/MerchandiseValidation.java
@@ -3,6 +3,7 @@ package validation;
 import entities.Merchandise;
 import org.hibernate.Session;
 import repositories.MerchandiseRepository;
+import utils.NumberHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +26,15 @@ public class MerchandiseValidation {
         if (merchandise.getType() == null || merchandise.getType().isEmpty()) {
             msg.add("Chưa điền loại");
         }
-        if (merchandise.getImportPrice() == null) {
+        if (merchandise.getImportPrice() == null || merchandise.getImportPrice().isEmpty()) {
             msg.add("Chưa điền giá mua");
+        } else if (!NumberHelper.isNumber(merchandise.getImportPrice())) {
+            msg.add("Giá mua phải là số");
         }
-        if (merchandise.getPrice() == null) {
+        if (merchandise.getPrice() == null || merchandise.getPrice().isEmpty()) {
             msg.add("Chưa điền giá bán");
+        } else if (!NumberHelper.isNumber(merchandise.getPrice())) {
+            msg.add("Giá bán phải là số");
         }
 
         return msg;
@@ -45,6 +50,22 @@ public class MerchandiseValidation {
             msg.add("Chưa điền tên");
         } else if (merchandiseName != null && merchandiseName.getId().equals(merchandise.getId())) {
             msg.add("Tên đã được sử dụng");
+        }
+        if (merchandise.getBranch() == null || merchandise.getBranch().isEmpty()) {
+            msg.add("Chưa điền thương hiệu");
+        }
+        if (merchandise.getType() == null || merchandise.getType().isEmpty()) {
+            msg.add("Chưa điền loại");
+        }
+        if (merchandise.getImportPrice() == null || merchandise.getImportPrice().isEmpty()) {
+            msg.add("Chưa điền giá mua");
+        } else if (!NumberHelper.isNumber(merchandise.getImportPrice())) {
+            msg.add("Giá mua phải là số");
+        }
+        if (merchandise.getPrice() == null || merchandise.getPrice().isEmpty()) {
+            msg.add("Chưa điền giá bán");
+        } else if (!NumberHelper.isNumber(merchandise.getPrice())) {
+            msg.add("Giá bán phải là số");
         }
 
         return msg;
