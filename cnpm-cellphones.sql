48d100bf9f8ea7a0f4df79ef52b9200cf91b79e8
Hải Huy <auhaihuy246@gmail.com>
2021-12-11 18:01:49 +0700

    [dev] [Update default db]

--- cnpm-cellphones.sql
+++ cnpm-cellphones.sql
@@ -41,20 +41,6 @@ CREATE TABLE `customer` (
 --
 -- Dumping data for table `customer`
 --
-
-INSERT INTO `customer` (`id`, `full_name`, `type`, `address`, `phone`, `email`, `created_date`, `updated_date`) VALUES
-('1305a70d-6a75-4b67-817b-a72639f07070', 'Cassandra R Santibanez', 'Khách hàng', '2133 Joanne Lane, Woburn, Massachusetts', '6179703129', 'dina2001@gmail.com', '2021-03-25', NULL),
-('17f95de3-3569-40dc-97a7-d6e8f4743f21', 'Huy', 'Nhà cung cấp', '555 Pasteur, Quận 8, TP.HCM', '0979487154', 'auhaihuy246@gmail.com', '2021-03-14', '2021-03-17'),
-('1b9cdded-1e5e-4e94-8c5e-fa5c4e44f3ce', 'Alfonso Jeffcoat', 'Nhà cung cấp', '2462 McVaney Road, Charlotte, North Carolina', '7045628176', 'carlos2003@yahoo.com', '2021-03-25', '2021-04-07'),
-('21e24bc6-31c6-48d2-8836-d0c57031be4a', 'sss', 'Khách hàng', '22', '1111111111', 'a@gmail.com', '2021-04-08', NULL),
-('245d626f-7bf0-4192-a857-7aab71e12697', 'John', 'Khách hàng', '221/3G Phan Văn Khoẻ, P.5, Q.6, TP.HCM', '0893012341', 'john@gmail.com', '2021-03-24', '2021-04-08'),
-('4ddb0779-2394-440a-b015-75d7a1290f2a', 'Hải', 'Khách hàng', '54A/87/22 Lê Trọng Tấn, BS Avenue, New York', '09798541258', 'hai@gmail.com', '2021-03-14', NULL),
-('510821e0-2347-4d45-b01f-25be3768a32e', 'John Bennet', 'Khách hàng', '1730 Roguski Road, Ruston', '9891436574', 'Bennet123@gmail.com', '2021-03-28', '2021-03-28'),
-('6322cea8-cc01-443e-a8b7-5dd465c4e86a', 'Oxy', 'Khách hàng', '221/3G Phan Văn Khoẻ, P.5, Q.6 TP.HCM', '0153485964', 'oXy@gmail.com', '2021-04-09', '2021-04-09'),
-('7f91758c-02ac-4538-a606-d9dbb47b5397', 'Winnie The Pooh', 'Khách hàng', 'High St, Hartfield TN7 4AE, United Kingdom', '1892771155', 'pooh@gmail.com', '2021-03-14', '2021-03-14'),
-('d52e95f1-b7c4-457d-9bda-a858c9911b63', 'Ronnie K Walters', 'Khách hàng', '1584 James Avenue, Wichita, Kansas', '3166802141', 'hazle2016@gmail.com', '2021-03-25', NULL),
-('f4f1c8df-8d9a-40bf-89c0-caf16347ed47', 'a', 'Khách hàng', 'aaaaa', '1234567891', 'asss@gmail.com', '2021-04-08', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -77,11 +63,7 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`id`, `full_name`, `birth_day`, `phone`, `email`, `password`, `created_date`, `updated_date`) VALUES
-('1a2e63bb-76d5-4375-9c84-96d246522224', 'Admin', '2021-03-19', '0908215136', 'admin@gmail.com', '$2a$09$OKsae4EItSmPiqZPkaXYIuYOM5kgVkhMbqxXjdMIv0LqA5RMN9A9u', '2021-03-18', NULL),
-('584ec37f-24e1-474e-8b3e-7339efebacd4', 'Sale account', '2021-04-23', '0945652541', 'sale@gmail.com', '$2a$09$kxG7hFRo2/5MwXRV5S3N1OwjgoiMr0cW7lMLdxJ4Y5NArDZu8pkvi', '2021-04-23', NULL),
-('63d962fe-7907-48c0-abe0-1a0c9bc48eed', 'Oxy', '2021-04-09', '0953625475', 'oxy@gmail.com', '$2a$09$jEnxi2Alp01iee0SadK38uC6UAdVMGnQBRTptnpOytIgaYUhVoSAW', '2021-04-09', NULL),
-('81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Fast Account', '2021-03-21', '0908706541', 'a', '$2a$09$ndFgZtuc4Q.s.xOJEGB6uOZ7xnFcySX.5DRATm65PDHrdgyGPuhry', '2021-03-21', NULL),
-('94485f3a-976b-4359-a41a-e4bea52ec5f5', 'ssss', '2021-04-06', '1111111111', 's@gma.com', '$2a$09$Lcrr0SWz6jHG4U6ZsUARPebwu3K8icFQNvEYm.MY1FRCv2yX5yjlG', '2021-04-08', NULL);
+('81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Admin', '2021-03-21', '0908706541', 'a', '$2a$09$ndFgZtuc4Q.s.xOJEGB6uOZ7xnFcySX.5DRATm65PDHrdgyGPuhry', '2021-03-21', NULL);
 
 -- --------------------------------------------------------
 
@@ -100,11 +82,7 @@ CREATE TABLE `employee_roles` (
 --
 
 INSERT INTO `employee_roles` (`id`, `roles_id`, `employee_id`) VALUES
-('00ab8d00-4c11-4708-9bdc-5521416088bb', 'c3faf74c-563b-48dc-9eaf-d5277273899e', '63d962fe-7907-48c0-abe0-1a0c9bc48eed'),
-('16cfc11d-a824-4d0a-95f0-f525988e8b5d', '5cac2fdb-a7ac-44ef-b2de-586f8f506637', '584ec37f-24e1-474e-8b3e-7339efebacd4'),
-('58bfa103-ea60-4384-a6e7-df54fb0a84f1', 'c3faf74c-563b-48dc-9eaf-d5277273899e', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783'),
-('b828ec0d-6c00-4762-ab12-b79251f19383', 'c3faf74c-563b-48dc-9eaf-d5277273899e', '1a2e63bb-76d5-4375-9c84-96d246522224'),
-('c5c7c64e-3cd6-4cc3-939a-88f6a4ca7f29', 'c3faf74c-563b-48dc-9eaf-d5277273899e', '94485f3a-976b-4359-a41a-e4bea52ec5f5');
+('58bfa103-ea60-4384-a6e7-df54fb0a84f1', 'c3faf74c-563b-48dc-9eaf-d5277273899e', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783');
 
 -- --------------------------------------------------------
 
@@ -125,11 +103,6 @@ CREATE TABLE `imports` (
 -- Dumping data for table `imports`
 --
 
-INSERT INTO `imports` (`id`, `orders_id`, `employee_id`, `description`, `created_date`, `updated_date`) VALUES
-('0bff0823-3497-4639-a2ae-a27fbc9199fe', '000b7b2d-ef8a-404f-b95c-14c12e6e356d', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', '2021-04-01', NULL),
-('a9639d3f-617b-4e9c-893f-96f1ef159c9f', '34b6849c-2456-465e-950c-96c17b6843cf', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Alfonso H Jeffcoat', '2021-04-01', NULL),
-('af43ac4c-8e3f-42c5-a6e9-d975a185e3b4', '000b7b2d-ef8a-404f-b95c-14c12e6e356d', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', '2021-04-01', NULL),
-('fc799f7e-3851-417d-9d15-80218e0c80a8', '65f17e68-d13b-46da-a4f5-439c4cafff7e', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', '2021-04-07', NULL);
 
 -- --------------------------------------------------------
 
@@ -149,13 +122,7 @@ CREATE TABLE `imports_detail` (
 -- Dumping data for table `imports_detail`
 --
 
-INSERT INTO `imports_detail` (`id`, `imports_id`, `merchandise_id`, `quantity`, `amount`) VALUES
-('1ddf35f8-e4ee-4b2f-a0ac-fb945e8b65dd', '0bff0823-3497-4639-a2ae-a27fbc9199fe', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 2, 5780000),
-('781a4af3-f273-44c7-b64a-97f44ec0e4cf', '0bff0823-3497-4639-a2ae-a27fbc9199fe', 'd458b1b7-95d1-429a-845e-36a78b7de133', 1, 21700000),
-('7db5ef58-45d6-4e15-872f-7acbfdd0b16c', 'af43ac4c-8e3f-42c5-a6e9-d975a185e3b4', '281a8ad7-f247-479b-9129-bed4dc0b6493', 1, 2790000),
-('7f8348bf-2a0f-41fe-a4d7-32ddbc0e9c9d', 'a9639d3f-617b-4e9c-893f-96f1ef159c9f', '421b6d29-f3c1-4cf3-9917-37fe0aa86ec8', 30, 1500000),
-('8b20263c-0da4-427e-b41f-c12e30f99c10', '0bff0823-3497-4639-a2ae-a27fbc9199fe', '281a8ad7-f247-479b-9129-bed4dc0b6493', 2, 5580000),
-('bc8ac610-fbd8-485f-867e-ef469dedb6e6', 'fc799f7e-3851-417d-9d15-80218e0c80a8', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 2, 5780000);
+
 
 -- --------------------------------------------------------
 
@@ -215,17 +182,6 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `type`, `customer_id`, `employee_id`, `description`, `status`, `created_date`, `updated_date`) VALUES
-('000b7b2d-ef8a-404f-b95c-14c12e6e356d', 'Nhập hàng', '17f95de3-3569-40dc-97a7-d6e8f4743f21', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', 'Hoàn tất', '2021-03-31', '2021-04-01'),
-('20e0ec8f-64bf-4e9d-9e64-fba185771faa', 'Bán hàng', '245d626f-7bf0-4192-a857-7aab71e12697', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng John mua hàng', 'Hoàn tất', '2021-03-26', '2021-03-26'),
-('34b6849c-2456-465e-950c-96c17b6843cf', 'Nhập hàng', '1b9cdded-1e5e-4e94-8c5e-fa5c4e44f3ce', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Alfonso H Jeffcoat', 'Chưa hoàn tất', '2021-04-01', NULL),
-('3f94a669-7115-4b58-82a4-8ba0d9b7f39c', 'Nhập hàng', '17f95de3-3569-40dc-97a7-d6e8f4743f21', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', 'Hoàn tất', '2021-03-26', '2021-03-26'),
-('58421cd1-ae5a-494d-aa3f-5d109a2e9cb1', 'Bán hàng', 'd52e95f1-b7c4-457d-9bda-a858c9911b63', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng Ronnie K Walters mua hàng', 'Hoàn tất', '2021-03-26', '2021-03-26'),
-('65f17e68-d13b-46da-a4f5-439c4cafff7e', 'Nhập hàng', '17f95de3-3569-40dc-97a7-d6e8f4743f21', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', 'Hoàn tất', '2021-03-27', '2021-04-07'),
-('89fc1e3a-f927-499d-ab54-2430ff4a06f0', 'Nhập hàng', '17f95de3-3569-40dc-97a7-d6e8f4743f21', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', 'Hoàn tất', '2021-03-26', '2021-03-26'),
-('96cc9fac-778a-4bf9-8e4c-f69f18756968', 'Bán hàng', '4ddb0779-2394-440a-b015-75d7a1290f2a', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng Hải mua hàng', 'Hoàn tất', '2021-03-27', '2021-04-07'),
-('e70d5e29-6abb-4dfe-9254-5d3c08f2a251', 'Bán hàng', '4ddb0779-2394-440a-b015-75d7a1290f2a', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng Hải mua hàng', 'Chưa hoàn tất', '2021-04-07', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -243,26 +199,6 @@ CREATE TABLE `orders_detail` (
 --
 -- Dumping data for table `orders_detail`
 --
-
-INSERT INTO `orders_detail` (`id`, `orders_id`, `merchandise_id`, `quantity`, `amount`) VALUES
-('2119e1c9-875d-4918-9c9f-adedcc38042b', '000b7b2d-ef8a-404f-b95c-14c12e6e356d', 'd458b1b7-95d1-429a-845e-36a78b7de133', 1, 21700000),
-('218cc914-77d1-4b8e-8be0-3f02d34d3df2', '89fc1e3a-f927-499d-ab54-2430ff4a06f0', 'd458b1b7-95d1-429a-845e-36a78b7de133', 1000, 2170000),
-('24214e8e-68c7-4bb5-9f32-5cc6687f8458', '3f94a669-7115-4b58-82a4-8ba0d9b7f39c', '3bf786c5-6ac4-41e0-bfd9-5750e3c191f4', 30, 164700000),
-('3c161948-a2db-4bc6-85ec-13e4917a5296', '3f94a669-7115-4b58-82a4-8ba0d9b7f39c', '6cee9f21-345e-4348-b516-da65870f1bed', 40, 120000000),
-('4269e428-9844-473a-9a59-8337ba510f9c', '3f94a669-7115-4b58-82a4-8ba0d9b7f39c', '123915d3-cef5-4168-98ab-2bd461ddb961', 30, 2147483647),
-('76b4f0d6-e769-4152-a0dc-a03356aaaa0e', '65f17e68-d13b-46da-a4f5-439c4cafff7e', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 2, 5780000),
-('92e44503-4abe-43a4-8ab7-aa0235e4abd7', '34b6849c-2456-465e-950c-96c17b6843cf', '407730fb-f46a-437b-9641-22c6f076696f', 20, 1000000),
-('98e094e8-884f-4a90-bae1-da55d921670a', '58421cd1-ae5a-494d-aa3f-5d109a2e9cb1', '123915d3-cef5-4168-98ab-2bd461ddb961', 1, 4890000),
-('9b9731f5-2267-4dfb-9d32-e8a608a10bde', '96cc9fac-778a-4bf9-8e4c-f69f18756968', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 1, 2890000),
-('a782cf4b-4c57-44d8-a8d1-289442b9f95b', '58421cd1-ae5a-494d-aa3f-5d109a2e9cb1', '64d812e0-ca72-4ea9-bf0e-bbe30017c5e4', 1, 550000),
-('a9e534e7-f0b2-46bc-9b51-41bb6ed909bc', '58421cd1-ae5a-494d-aa3f-5d109a2e9cb1', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 1, 2890000),
-('aaa38f25-00dd-4174-ae32-87d4cba4789c', '89fc1e3a-f927-499d-ab54-2430ff4a06f0', '329d09d1-0483-4459-865f-5d6dd5700ddb', 50, 124500000),
-('db3404aa-51ec-4458-b520-c74d5d9affce', '20e0ec8f-64bf-4e9d-9e64-fba185771faa', '281a8ad7-f247-479b-9129-bed4dc0b6493', 2, 5580000),
-('e571d2f0-ea26-4e04-b7cc-a077010e7b10', '000b7b2d-ef8a-404f-b95c-14c12e6e356d', '281a8ad7-f247-479b-9129-bed4dc0b6493', 3, 8370000),
-('e5813016-54a6-4d0d-9c3d-ded7ba364c7a', '34b6849c-2456-465e-950c-96c17b6843cf', '421b6d29-f3c1-4cf3-9917-37fe0aa86ec8', 30, 1500000),
-('ebab5777-fe21-4c1a-b80f-4e27a4002b30', '000b7b2d-ef8a-404f-b95c-14c12e6e356d', 'ef41a828-c592-4c4f-a218-71ebfbd2a5ad', 2, 5780000),
-('f3f5e037-33dd-464e-9608-2e5b00d4acfd', 'e70d5e29-6abb-4dfe-9254-5d3c08f2a251', '6cee9f21-345e-4348-b516-da65870f1bed', 2, 6000000);
-
 -- --------------------------------------------------------
 
 --
@@ -288,8 +224,7 @@ INSERT INTO `permissions` (`code`, `name`, `created_date`, `updated_date`) VALUE
 ('ORDER_MANAGEMENT', 'Quản lí đơn hàng', NULL, NULL),
 ('RECEIPT_MANAGEMENT', 'Quản lí hoá đơn', NULL, NULL),
 ('ROLES_MANAGEMENT', 'Quản lí chức vụ', NULL, NULL),
-('STATISTIC', 'Thống kê', NULL, NULL),
-('WORK_SHIFT_MANAGEMENT', 'Quản lí lịch làm', NULL, NULL);
+('STATISTIC', 'Thống kê', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -310,13 +245,6 @@ CREATE TABLE `receipt` (
 -- Dumping data for table `receipt`
 --
 
-INSERT INTO `receipt` (`id`, `order_id`, `employee_id`, `description`, `created_date`, `updated_date`) VALUES
-('093e04d0-7097-4157-a630-26a1c3b9fc7b', '96cc9fac-778a-4bf9-8e4c-f69f18756968', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng Hải mua hàng', '2021-04-07', NULL),
-('2675d78c-f2ba-4ea0-8195-ea41c30989c6', '89fc1e3a-f927-499d-ab54-2430ff4a06f0', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', '2021-03-26', NULL),
-('581f640c-6f0c-40ba-a21b-74174b209804', '20e0ec8f-64bf-4e9d-9e64-fba185771faa', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng John mua hàng', '2021-03-26', NULL),
-('8dfbce98-f1f1-41d4-b529-2f18c96af898', '3f94a669-7115-4b58-82a4-8ba0d9b7f39c', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Mua hàng từ nhà cung cấp Huy', '2021-03-26', NULL),
-('9c8c8503-dcc6-4f3b-a37f-091334a8e769', '58421cd1-ae5a-494d-aa3f-5d109a2e9cb1', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'Khách hàng Ronnie K Walters mua hàng', '2021-03-26', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -335,9 +263,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_date`, `updated_date`) VALUES
-('5cac2fdb-a7ac-44ef-b2de-586f8f506637', 'Sale', '2021-03-18', NULL),
-('c3faf74c-563b-48dc-9eaf-d5277273899e', 'Admin', '2021-03-18', NULL),
-('cae80e8f-d562-445b-a502-487c91dccf37', 'Admin Sale', '2021-03-18', NULL);
+('c3faf74c-563b-48dc-9eaf-d5277273899e', 'Admin', '2021-03-18', NULL);
 
 -- --------------------------------------------------------
 
@@ -356,25 +282,14 @@ CREATE TABLE `roles_detail` (
 --
 
 INSERT INTO `roles_detail` (`id`, `roles_id`, `permission_code`) VALUES
-('02c2e789-8b79-49d4-968e-5d8a6a85bf9d', '5cac2fdb-a7ac-44ef-b2de-586f8f506637', 'ORDER_MANAGEMENT'),
-('10544dc7-3e75-4b7a-840d-0150dc0f6343', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'WORK_SHIFT_MANAGEMENT'),
 ('2d580a4f-b783-42b6-a28b-854004601ff7', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'RECEIPT_MANAGEMENT'),
-('348cb263-9f28-4c50-ac77-2b52ba10f0b3', 'cae80e8f-d562-445b-a502-487c91dccf37', 'ORDER_MANAGEMENT'),
 ('539903c5-a678-4970-aa81-f68e63e04241', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'EMPLOYEE_MANAGEMENT'),
 ('6a2903ab-6973-4abc-8b28-318afdab70dd', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'MERCHANDISE_MANAGEMENT'),
 ('6e441ea0-289a-429b-8486-717fd101ac0a', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'ORDER_MANAGEMENT'),
-('7c7944b4-f329-4abf-8433-810e9aaf33e8', 'cae80e8f-d562-445b-a502-487c91dccf37', 'RECEIPT_MANAGEMENT'),
-('8803db8b-1fcc-4580-898e-31df4ab0c0b8', 'cae80e8f-d562-445b-a502-487c91dccf37', 'WORK_SHIFT_MANAGEMENT'),
-('90be569c-34e5-4580-beb4-c8e8329d949b', 'cae80e8f-d562-445b-a502-487c91dccf37', 'ORDER_MANAGEMENT'),
 ('9a8f9ddd-9b0e-4019-a23e-9040edc98c82', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'STATISTIC'),
-('b08a14e3-c88f-4cbb-9ca5-60211d1ff61c', 'cae80e8f-d562-445b-a502-487c91dccf37', 'WORK_SHIFT_MANAGEMENT'),
 ('b10dafe9-79a3-42fa-b668-ada88a9b74fe', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'IMPORT_MANAGEMENT'),
 ('b4949ea9-ba7d-40da-bbbd-cf7b6a3138d7', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'ROLES_MANAGEMENT'),
-('b6f92abd-8fac-4189-beae-6fbbedeb7484', 'cae80e8f-d562-445b-a502-487c91dccf37', 'MERCHANDISE_MANAGEMENT'),
-('b9bc5841-694b-4342-9116-a5ae77a72393', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'CUSTOMER_MANAGEMENT'),
-('bf50c104-4381-496b-a865-f591e22c1f4a', 'cae80e8f-d562-445b-a502-487c91dccf37', 'RECEIPT_MANAGEMENT'),
-('d957b1d3-4103-4bfe-b189-4873c848ada7', 'cae80e8f-d562-445b-a502-487c91dccf37', 'CUSTOMER_MANAGEMENT'),
-('fa0fbbb8-2c05-40ed-8184-cdfc00d4f5c1', 'cae80e8f-d562-445b-a502-487c91dccf37', 'MERCHANDISE_MANAGEMENT');
+('b9bc5841-694b-4342-9116-a5ae77a72393', 'c3faf74c-563b-48dc-9eaf-d5277273899e', 'CUSTOMER_MANAGEMENT');
 
 -- --------------------------------------------------------
 
@@ -395,11 +310,6 @@ CREATE TABLE `work_shift` (
 -- Dumping data for table `work_shift`
 --
 
-INSERT INTO `work_shift` (`id`, `name`, `start_time`, `end_time`, `created_date`, `updated_date`) VALUES
-('2f6653c1-8806-49d4-8a7b-2083485f061c', 'Trưa', '13:00', '17:30', '2021-04-09', '2021-04-09'),
-('40fbc38b-7811-4f2c-90f7-8b1fec34995c', 'Tối', '17:30', '22:00', '2021-04-23', NULL),
-('d07297f2-5b82-4d96-9a2e-7dda13dd1596', 'Sáng', '7:00', '11:00', '2021-04-09', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -416,12 +326,6 @@ CREATE TABLE `work_table` (
 --
 -- Dumping data for table `work_table`
 --
-
-INSERT INTO `work_table` (`id`, `employee_id`, `shift_id`, `day_of_week`) VALUES
-('03d06401-05ec-4ccb-a0de-fa6f13c08852', '94485f3a-976b-4359-a41a-e4bea52ec5f5', '2f6653c1-8806-49d4-8a7b-2083485f061c', 'T3-T7'),
-('0f7e15c1-47c4-4321-8aec-4362ef17ff22', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', '2f6653c1-8806-49d4-8a7b-2083485f061c', 'T2-T3-T4-T7'),
-('b1eeac0e-0799-48b9-891c-a8641608d540', '94485f3a-976b-4359-a41a-e4bea52ec5f5', '40fbc38b-7811-4f2c-90f7-8b1fec34995c', 'T3'),
-('ca39a04a-3614-485a-9a37-537f26f6126f', '81915f39-dd3e-42e7-8db0-d7dc8bb3f783', 'd07297f2-5b82-4d96-9a2e-7dda13dd1596', 'T2-T6');
 
 --
 -- Indexes for dumped tables
